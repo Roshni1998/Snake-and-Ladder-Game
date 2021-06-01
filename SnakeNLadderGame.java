8201f96c01c6793cdbb1c30656e2ebfa662160be
Roshni1998 <roshnimali847@gmail.com>
2021-06-01 14:46:43 +0530

    Added: Welcome to Snake and Ladder Game

---
+++ SnakeNLadderGame.java
@@ -0,0 +1,9 @@
+package com.SnakeAndLadder;
+
+public class SnakeNLadderGame {
+	public static void main(String[] args) {
+		int playerPosition = 0;
+		System.out.println("Welcome to the Game of SNAKE & LADDER");
+		System.out.println("Starting Position of the Player = " + playerPosition);
+	}
+}
