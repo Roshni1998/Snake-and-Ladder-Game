8ed04f5158ad17a87c4770d74e4a92d2db3b1ba8
Roshni1998 <roshnimali847@gmail.com>
2021-06-01 14:52:53 +0530

    Added: SnakeLadder UC-1

--- SnakeNLadderGame.java
+++ SnakeNLadderGame.java
@@ -1,9 +1,15 @@
 package com.SnakeAndLadder;
 
 public class SnakeNLadderGame {
+
 	public static void main(String[] args) {
 		int playerPosition = 0;
+		int diceRoll = 0;
 		System.out.println("Welcome to the Game of SNAKE & LADDER");
 		System.out.println("Starting Position of the Player = " + playerPosition);
+		double randomnumber = (double) (Math.random() * 6) + 1;
+		diceRoll = (int) randomnumber;
+		System.out.println("Dice Number =" + " " + diceRoll);
+
 	}
 }
